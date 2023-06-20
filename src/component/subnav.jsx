dff26463b0e19b377a9a04ac0818cdbcdceb6e07
Mahmoud Abd Elaziz <mahmoudabdelaziz93.mz@gmail.com>
2023-06-21 01:07:19 +0300

    Creat SubNav  Component

---
+++ src/component/subnav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Subnav = () => {
+    return (  <>
+         <nav id="SubNav" className="bg-slate-400">
+        <div className="flex justify-evenly p-3 ">
+          <span>Mobile</span>
+          <span>Computer</span>
+          <span>Labtop</span>
+          <span>cars</span>
+        </div>
+      </nav>
+    </>);
+}
+ 
+export default Subnav;
