d5b18128b8a886e547ea4e48fb9364d4b9dba8be
Mahmoud Abd Elaziz <mahmoudabdelaziz93.mz@gmail.com>
2023-06-24 20:33:22 +0300

    Add State and Handeler to SearchBar

--- src/component/searchbar.jsx
+++ src/component/searchbar.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
 
 const Searchbar = () => {
+   const [searchText, setSearchText] = useState("");
+  
+  const changeHandler = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const clickHandler = (e, searchClick) => {
+    console.log(searchClick);
+  };
+
   return (
     <>
       <div className="flex items-center justify-center col-span-2">
         <div className="flex space-x-1">
           <input
+            onChange={changeHandler}
             type="text"
+            // value={this.target.value}
             className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             placeholder="Search..."
           />
-          <button className="px-4 text-white bg-purple-600 rounded-full ">
+          <button
+            onClick={(e) => clickHandler(e, searchText)}
+            className="px-4 text-white bg-purple-600 rounded-full "
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 h-5"
@@ -33,3 +49,6 @@ const Searchbar = () => {
 };
 
 export default Searchbar;
+
+
+
