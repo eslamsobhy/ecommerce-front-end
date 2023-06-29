5e49e1055fae68c6065a58a337f6b0631f9759ef
asmaatajeldein <asmaatajeldein@gmail.com>
2023-06-29 11:52:37 +0300

    modify filter to be responsive

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,6 +5,7 @@ const brands = ["All", "Brand A", "Brand B", "Brand C"]; // Replace with your br
 
 const Filter = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isOpenBrand, setIsOpenBrand] = useState(false);
   const [isOpenPrice, setIsOpenPrice] = useState(false);
   const [selectedBrand, setSelectedBrand] = useState(
@@ -15,12 +16,20 @@ const Filter = () => {
     max: searchParams.get("max") ? searchParams.get("max") : ""
   });
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const toggleBrandSection = () => {
     setIsOpenBrand(!isOpenBrand);
   };
 
   const togglePriceSection = () => {
     setIsOpenPrice(!isOpenPrice);
+  };
+
+  const handleBackdropClick = () => {
+    setIsMenuOpen(false);
   };
 
   const handlePriceChange = (e, type) => {
@@ -45,66 +54,98 @@ const Filter = () => {
     }
 
     setSearchParams(data);
+    setIsMenuOpen(false);
   };
 
   return (
-    <div className="fixed top-0 left-0 w-80 p-4 bg-white shadow-md rounded-md">
-      <button
-        className="flex items-center justify-between w-full px-4 py-2 text-lg font-medium text-left bg-gray-200 focus:outline-none focus:bg-gray-300 rounded-md"
-        onClick={toggleBrandSection}
+    <>
+      <div className="md:hidden block fixed bottom-4 left-4">
+        <button
+          className="rounded-full bg-gray-200 p-3 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <FilterIcon />
+        </button>
+      </div>
+      <div
+        className={
+          !isMenuOpen
+            ? "fixed top-0 left-0 hidden md:block md:w-80 p-4 bg-white shadow-md rounded-md"
+            : "relative left-0 top-0 h-screen w-64 p-4 bg-white shadow-lg"
+        }
       >
-        Brands
-        <ArrowIcon isOpen={isOpenBrand} />
-      </button>
-      {isOpenBrand && (
-        <div className="mt-4">
-          {brands.map((brand) => (
-            <div key={brand} className="space-x-2">
-              <input
-                type="radio"
-                checked={selectedBrand === brand}
-                onChange={() => handleBrandChange(brand)}
-                name={brand}
-              />
-              <label htmlFor={brand}>{brand}</label>
-            </div>
-          ))}
-        </div>
-      )}
-      <button
-        className="flex items-center justify-between w-full px-4 py-2 mt-4 text-lg font-medium text-left bg-gray-200 focus:outline-none focus:bg-gray-300 rounded-md"
-        onClick={togglePriceSection}
-      >
-        Price Range
-        <ArrowIcon isOpen={isOpenPrice} />
-      </button>
-      {isOpenPrice && (
-        <div className="mt-4">
-          <div className="flex space-x-2">
-            <input
-              type="number"
-              placeholder="Min"
-              value={priceRange.min}
-              onChange={(e) => handlePriceChange(e, "min")}
-              className="w-1/2 px-2 py-1 text-sm border rounded-md"
-            />
-            <input
-              type="number"
-              placeholder="Max"
-              value={priceRange.max}
-              onChange={(e) => handlePriceChange(e, "max")}
-              className="w-1/2 px-2 py-1 text-sm border rounded-md"
-            />
+        {/* Add Close Button to Menu */}
+        {isMenuOpen && (
+          <div className="flex justify-end pb-3">
+            <span className="cursor-pointer" onClick={toggleMenu}>
+              <CloseIcon />
+            </span>
           </div>
-        </div>
+        )}
+
+        <button
+          className="flex items-center justify-between w-full px-4 py-2 text-lg font-medium text-left bg-gray-200 focus:outline-none focus:bg-gray-300 rounded-md"
+          onClick={toggleBrandSection}
+        >
+          Brands
+          <ArrowIcon isOpen={isOpenBrand} />
+        </button>
+        {isOpenBrand && (
+          <div className="mt-4">
+            {brands.map((brand) => (
+              <div key={brand} className="space-x-2">
+                <input
+                  type="radio"
+                  checked={selectedBrand === brand}
+                  onChange={() => handleBrandChange(brand)}
+                  name={brand}
+                />
+                <label htmlFor={brand}>{brand}</label>
+              </div>
+            ))}
+          </div>
+        )}
+        <button
+          className="flex items-center justify-between w-full px-4 py-2 mt-4 text-lg font-medium text-left bg-gray-200 focus:outline-none focus:bg-gray-300 rounded-md"
+          onClick={togglePriceSection}
+        >
+          Price Range
+          <ArrowIcon isOpen={isOpenPrice} />
+        </button>
+        {isOpenPrice && (
+          <div className="mt-4">
+            <div className="flex space-x-2">
+              <input
+                type="number"
+                placeholder="Min"
+                value={priceRange.min}
+                onChange={(e) => handlePriceChange(e, "min")}
+                className="w-1/2 px-2 py-1 text-sm border rounded-md"
+              />
+              <input
+                type="number"
+                placeholder="Max"
+                value={priceRange.max}
+                onChange={(e) => handlePriceChange(e, "max")}
+                className="w-1/2 px-2 py-1 text-sm border rounded-md"
+              />
+            </div>
+          </div>
+        )}
+        <button
+          className="w-full px-4 py-2 mt-4 text-sm font-medium text-white bg-orange-500 rounded-md hover:bg-orange-600"
+          onClick={applyFilters}
+        >
+          Apply Filters
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div
+          className="fixed inset-y-0 right-0 w-[calc(100%-16rem)] bg-gray-900 opacity-50"
+          onClick={handleBackdropClick}
+        />
       )}
-      <button
-        className="w-full px-4 py-2 mt-4 text-sm font-medium text-white bg-orange-500 rounded-md hover:bg-orange-600"
-        onClick={applyFilters}
-      >
-        Apply Filters
-      </button>
-    </div>
+    </>
   );
 };
 
@@ -124,6 +165,48 @@ const ArrowIcon = ({ isOpen }) => {
           strokeLinejoin="round"
           strokeWidth="2"
           d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </>
+  );
+};
+
+const FilterIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+        />
+      </svg>
+    </>
+  );
+};
+
+const CloseIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18L18 6M6 6l12 12"
         />
       </svg>
     </>
