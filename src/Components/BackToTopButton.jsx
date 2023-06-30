166d9e791d8e722c2042364eeea2e9ce616c449c
Mahmoud Abd Elaziz <mahmoudabdelaziz93.mz@gmail.com>
2023-06-30 18:14:47 +0300

    Create Footer

---
+++ src/Components/BackToTopButton.jsx
@@ -0,0 +1,16 @@
+const BackToTopButton = () => {
+    const handleBackToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+  
+    return (
+      <button
+        className="bg-gray-400 text-white px-4 py-2 rounded-full fixed bottom-4 right-4"
+        onClick={handleBackToTop}
+      >
+        <img className="w-5 h-5" src="../../public/up-arrow-svgrepo-com.svg"></img>
+      </button>
+    );
+  };
+  
+  export default BackToTopButton;
