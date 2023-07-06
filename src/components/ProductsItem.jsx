79d4265d7c9ee9c31c8f78cdc2a5c19295963a71
asmaatajeldein <asmaatajeldein@gmail.com>
2023-07-06 16:55:43 +0300

    add functionality to add to cart button for product item

--- src/components/ProductsItem.jsx
+++ src/components/ProductsItem.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { CartIcon } from "./Icons";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import CartContext from "../context/CartContext";
 
 const ProductsItem = ({ item }) => {
+  const myCart = useContext(CartContext);
+  function addItemToCart(product) {
+    myCart.addItem({
+      key: product._id,
+      id: product._id,
+      name: product.name,
+      image: product.images[0].url,
+      amount: 1,
+      price: product.new_price ?? product.price
+    });
+  }
   return (
     <>
-      <Link to={`/products/${item.id}`}>
-        <div className="item p-3 m-2 border border-slate rounded-lg hover:shadow-lg transition-shadow duration-300">
+      <div className="item p-3 m-2 border border-slate rounded-lg hover:shadow-lg transition-shadow duration-300">
+        <Link to={`/products/${item.id}`}>
           <div className="relative">
             <img
               src={item.images[0].url}
@@ -26,12 +39,15 @@ const ProductsItem = ({ item }) => {
           <h5 className="my-2 text-gray-700 text-center text-2xl">
             EGP{item.price}
           </h5>
-          <button className="flex items-center border border-slate rounded-lg px-2 mx-auto bg-gray-100 hover:bg-orange-500 hover:text-white transition-all duration-300">
-            <CartIcon />
-            <span>Add to Cart</span>
-          </button>
-        </div>
-      </Link>
+        </Link>
+        <button
+          onClick={() => addItemToCart(item)}
+          className="flex items-center border border-slate rounded-lg px-2 mx-auto bg-gray-100 hover:bg-orange-500 hover:text-white transition-all duration-300"
+        >
+          <CartIcon />
+          <span>Add to Cart</span>
+        </button>
+      </div>
     </>
   );
 };
