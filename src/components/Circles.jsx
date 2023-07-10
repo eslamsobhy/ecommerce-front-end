016bc0d5f90d6bac7d2a6c86682c09de1636b4f0
asmaatajeldein <asmaatajeldein@gmail.com>
2023-07-10 21:10:41 +0300

    modify links of circles

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -8,50 +8,50 @@ function Circles() {
     <>
       <div className="flex justify-center items-center mx-12">
         <div className="flex flex-wrap justify-center gap-12 max-w-5xl">
-          <Link to="/products/?orderBy=bestSeller">
+          <Link to="/products?orderBy=bestSeller">
             <Circle
               srcImg={"/public/circles-images/best-sellers.png"}
               title={"Best Sellers"}
             />
           </Link>
 
-          <Link to="/products/?orderBy=newArrivals">
+          <Link to="/products?orderBy=newArrivals">
             <Circle
               srcImg={"/public/circles-images/new-arrivals.png"}
               title={"New Arrivals"}
             />
           </Link>
 
-          <Link to="/products/?orderBy=hasOffer">
+          <Link to="/products?orderBy=hasOffer">
             <Circle
               srcImg={"/public/circles-images/offers.png"}
               title={"Offers"}
             />
           </Link>
 
-          <Link to="/products/?category=TVs">
+          <Link to="/products?category=TVs">
             <Circle srcImg={"/public/circles-images/tvs.png"} title={"TVs"} />
           </Link>
 
-          <Link to="/products/?category=Laptops">
+          <Link to="/products?category=Laptops">
             <Circle
               srcImg={"/public/circles-images/laptops.png"}
               title={"Laptops"}
             />
           </Link>
-          <Link to="/products/?category=Mobiles">
+          <Link to="/products?category=Mobiles">
             <Circle
               srcImg={"/public/circles-images/mobiles.png"}
               title={"Mobiles"}
             />
           </Link>
-          <Link to="/products/?category=Gaming">
+          <Link to="/products?category=Gaming">
             <Circle
               srcImg={"/public/circles-images/gaming.png"}
               title={"Gaming"}
             />
           </Link>
-          <Link to="/products/?category=Accessories">
+          <Link to="/products?category=Accessories">
             <Circle
               srcImg={"/public/circles-images/accessories.png"}
               title={"Accessories"}
