879fecf43e1e6aa5d85249d76647dfed15540f35
asmaatajeldein <asmaatajeldein@gmail.com>
2023-07-10 21:12:30 +0300

    modify the style of services buttons

--- src/components/OtherServices.jsx
+++ src/components/OtherServices.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { FaTruckPickup  } from 'react-icons/fa';
-import { MdOutlineMiscellaneousServices  } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { FaTruckPickup } from "react-icons/fa";
+import { MdOutlineMiscellaneousServices } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 export default function OtherServices() {
   const [isHovered1, setIsHovered1] = useState(false);
@@ -26,29 +26,33 @@ export default function OtherServices() {
   return (
     <div className="flex max-w-[900px] m-auto px-12 my-12 justify-around flex-wrap">
       <Link to="/Techservices">
-      <div
-        className={`flex items-center font-bold bg-slate-300 hover:bg-orange-500 text-white ${
-          isHovered1 ? 'bg-orange-500 text-white' : ''
-        } w-[270px] cursor-pointer rounded-md px-4 justify-center mx-5 my-1 transition duration-300 py-3`}
-        onMouseEnter={handleMouseEnter1}
-        onMouseLeave={handleMouseLeave1}
+        <div
+          className={`flex items-center font-bold bg-black hover:bg-orange-500 text-white ${
+            isHovered1 ? "bg-orange-500 text-white" : ""
+          } w-[270px] cursor-pointer rounded-md px-4 justify-center mx-5 my-1 transition duration-300 py-3`}
+          onMouseEnter={handleMouseEnter1}
+          onMouseLeave={handleMouseLeave1}
         >
-        {isHovered1 ? 'Learn more' : 'Technical Service'}
-        <MdOutlineMiscellaneousServices className='ml-3' size={32} color={isHovered1 ? 'white' : '#f37020'} />
-      </div>
-        </Link>
+          {isHovered1 ? "Learn more" : "Technical Service"}
+          <MdOutlineMiscellaneousServices
+            className="ml-3"
+            size={32}
+            color="white"
+          />
+        </div>
+      </Link>
 
       <Link to="/Freeshipping">
-      <div
-        className={`flex items-center font-bold bg-slate-300 hover:bg-orange-500 text-white ${
-          isHovered2 ? 'bg-orange-500 text-white' : ''
-        } w-[270px] cursor-pointer rounded-md px-4 justify-center mx-5 my-1 transition duration-300 py-3`}
-        onMouseEnter={handleMouseEnter2}
-        onMouseLeave={handleMouseLeave2}
-      >
-        {isHovered2 ? 'Learn more' : 'Free Shipping'}
-        <FaTruckPickup className='ml-3' size={32} color={isHovered2 ? 'white' : '#f37020'}/>
-      </div>
+        <div
+          className={`flex items-center font-bold bg-black hover:bg-orange-500 text-white ${
+            isHovered2 ? "bg-orange-500 text-white" : ""
+          } w-[270px] cursor-pointer rounded-md px-4 justify-center mx-5 my-1 transition duration-300 py-3`}
+          onMouseEnter={handleMouseEnter2}
+          onMouseLeave={handleMouseLeave2}
+        >
+          {isHovered2 ? "Learn more" : "Free Shipping"}
+          <FaTruckPickup className="ml-3" size={32} color="white" />
+        </div>
       </Link>
     </div>
   );
