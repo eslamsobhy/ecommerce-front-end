76ee7e885489a66d0c793e531409467d1098b47b
asmaatajeldein <asmaatajeldein@gmail.com>
2023-07-12 18:58:22 +0300

    add links to product route

--- src/components/Product/ProductRoute.jsx
+++ src/components/Product/ProductRoute.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { HomeIcon, RightArrowIcon } from "../Icons";
 
 const ProductRoute = ({ product }) => {
@@ -5,10 +7,18 @@ const ProductRoute = ({ product }) => {
   return (
     <>
       <div className="flex flex-wrap text-sm font-bold text-gray-800 lg:justify-center lg:items-center p-5">
-        <HomeIcon />
+        <Link to="/">
+          <HomeIcon />
+        </Link>
+
         <RightArrowIcon />
-        <span className="pb-[2px]">{product.category_id.category_name}</span>
+
+        <Link to={`/products?category=${product.category_id.category_name}`}>
+          <span className="pb-[2px]">{product.category_id.category_name}</span>
+        </Link>
+
         <RightArrowIcon />
+
         <span className="pb-[2px] break-words">{product.name}</span>
       </div>
     </>
